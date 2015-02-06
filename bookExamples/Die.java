31acb2bceb5c4b24847c6c22082f5ca7a8ab354c
Zach <tinvariable578@gmail.com>
2015-02-06 13:57:44 -0700

    Examples Done

---
+++ bookExamples/Die.java
@@ -0,0 +1,32 @@
+package bookExamples;
+
+public class Die {
+
+		private final int MAX = 6;
+		private int faceValue;
+		
+		public Die()
+		{
+			faceValue = 1;
+		}
+		public int roll()
+		{
+			faceValue = (int) (Math.random() * MAX) +1;
+			return faceValue;
+		}
+		public void setFaceValue(int value)
+		{
+			if (value < 0 && value <= MAX)
+				faceValue = value;
+		}
+		public int getFaceValue()
+		{
+			return faceValue;
+		}
+		public String toString()
+		{
+			String result = Integer.toString(faceValue);
+			return result;
+		}
+		
+}
